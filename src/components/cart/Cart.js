cdcccdbae97bc1243574d170d843051eba46b600
AR Nabil <nabil.oqtave@gmail.com>
2020-08-27 22:34:05 +0600

    finish

---
+++ src/components/cart/Cart.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '.cart/Cart.css';
+import Items from './items/Items';
+
+const Cart = () => {
+   
+    return (
+       
+        <div className="header-start">
+            <div className="cart">
+            <h1>this is cart</h1>
+            </div>
+        <div className="header-area">
+            <h1>Our featured courses</h1>
+        </div>
+        </div>
+    );
+
+};
+
+export default Cart;
